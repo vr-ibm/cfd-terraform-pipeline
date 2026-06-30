0ff2c4605c8d1727ca16440bb10a808ad1bcc60d
vr-ibm <vrau@ibm.com>
2026-06-30 11:24:15 -0500

    feat: add Artifact Registry for OpenFOAM container images

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,2 +1,6 @@
 # outputs.tf - output value definitions
-# TODO: implement
+
+output "container_registry_path" {
+	description = "Base path for pushing container images"
+	value       = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.cfd.repository_id}"
+}
