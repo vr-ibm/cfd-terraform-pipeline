bb6e456e8644529c99c4f423cb39bdb307efa474
vr-ibm <vrau@ibm.com>
2026-06-30 11:37:30 -0500

    feat: add Cloud Batch job definition with parametric sweep support

--- terraform/batch.tf
+++ terraform/batch.tf
@@ -1,2 +1,86 @@
 # batch.tf - Cloud Batch resources
-# TODO: implement
+
+resource "google_batch_job" "cfd_run" {
+	for_each = { for c in var.cases : c.name => c }
+
+	name     = each.value.name
+	location = var.region
+	project  = var.project_id
+
+	task_groups {
+		task_count = 1
+
+		task_spec {
+			compute_resource {
+				cpu_milli  = 16000
+				memory_mib = 32768
+			}
+
+			max_retry_count  = 1
+			max_run_duration = "3600s"
+
+			runnables {
+				container {
+					image_uri  = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.cfd.repository_id}/openfoam-cfd:${var.container_image_tag}"
+					entrypoint = "/opt/cfd/entrypoint.sh"
+				}
+
+				environment {
+					variables = {
+						CASE_NAME         = each.value.name
+						AIRFOIL           = each.value.airfoil
+						AOA               = tostring(each.value.aoa)
+						REYNOLDS          = tostring(each.value.reynolds)
+						MACH              = tostring(each.value.mach)
+						GCS_INPUT_BUCKET  = google_storage_bucket.input.name
+						GCS_OUTPUT_BUCKET = google_storage_bucket.output.name
+						MACHINE_TYPE      = var.machine_type
+					}
+				}
+			}
+		}
+	}
+
+	allocation_policy {
+		instances {
+			policy {
+				machine_type       = var.machine_type
+				provisioning_model = "STANDARD"
+			}
+		}
+
+		network {
+			network_interfaces {
+				network                = google_compute_network.cfd_hpc.id
+				subnetwork             = google_compute_subnetwork.cfd_hpc.id
+				no_external_ip_address = true
+			}
+		}
+
+		service_account {
+			email  = google_service_account.batch_runner.email
+			scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+		}
+
+		location {
+			allowed_locations = ["zones/${var.zone}"]
+		}
+	}
+
+	logs_policy {
+		destination = "CLOUD_LOGGING"
+	}
+
+	labels = {
+		project = "cfd-pipeline"
+		case    = each.value.name
+		airfoil = each.value.airfoil
+	}
+
+	depends_on = [
+		google_project_service.required["batch.googleapis.com"],
+		google_artifact_registry_repository_iam_member.batch_pull,
+		google_storage_bucket_iam_member.batch_input_read,
+		google_storage_bucket_iam_member.batch_output_write,
+	]
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -17,3 +17,35 @@ variable "zone" {
 	type        = string
 	default     = "us-central1-a"
 }
+
+variable "cases" {
+	description = "List of CFD simulation cases to run"
+	type = list(object({
+		name     = string
+		airfoil  = string
+		aoa      = number
+		reynolds = number
+		mach     = number
+	}))
+	default = [
+		{ name = "naca0012_aoa0", airfoil = "naca0012", aoa = 0, reynolds = 3000000, mach = 0.15 },
+		{ name = "naca0012_aoa2", airfoil = "naca0012", aoa = 2, reynolds = 3000000, mach = 0.15 },
+		{ name = "naca0012_aoa4", airfoil = "naca0012", aoa = 4, reynolds = 3000000, mach = 0.15 },
+		{ name = "naca0012_aoa6", airfoil = "naca0012", aoa = 6, reynolds = 3000000, mach = 0.15 },
+		{ name = "naca0012_aoa8", airfoil = "naca0012", aoa = 8, reynolds = 3000000, mach = 0.15 },
+		{ name = "naca0012_aoa10", airfoil = "naca0012", aoa = 10, reynolds = 3000000, mach = 0.15 },
+		{ name = "naca0012_aoa12", airfoil = "naca0012", aoa = 12, reynolds = 3000000, mach = 0.15 },
+	]
+}
+
+variable "machine_type" {
+	description = "GCP machine type for Batch compute VMs"
+	type        = string
+	default     = "c3-highcpu-22"
+}
+
+variable "container_image_tag" {
+	description = "Tag for the OpenFOAM container image"
+	type        = string
+	default     = "latest"
+}
